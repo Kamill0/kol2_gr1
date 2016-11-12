967eb6f75f37da38f81bc81a65b91edd8bd64c9f
Kamill0 <kamillo1493@gmail.com>
2016-11-13 00:39:15 +0100

    Improvements

--- diary.py
+++ diary.py
@@ -1,39 +1,101 @@
 from subject import Subject
 from student import Student
+from pprint import pprint
+import re
+import json
 
 class Diary(object):
+	MENU = ["\n************Main Menu************\n1. Add a student\n2. Delete student\n3. Display all students\n4. Select student\n5. Exit\nWhat would You like to do? ", "\n************Student Menu************\n1. Get total average\n2. Get subject average\n3. Get total attendance\n4. Get subject attendance\n5. Return\nWhat would You like to do? ", "\n*********************************\n"]
+
 	"""Class represents a school diary"""
-	def __init__(self, school_name, year, students = []):
+	def __init__(self, school_name, year, filename):
 		"""Default init method"""
 		self.school_name = school_name
-		self.year = year			
-		self.students = students
+		self.year = year
+		self.filename = filename
+		self.json_data = {}
+		self.students = self.__load_data(self.filename)		
+
+	def __load_data(self, filename):
+		"""Loads data from JSON file"""
+		with open(filename) as data_file:    
+    			self.json_data = json.load(data_file)
+		return [Student(stud, [Subject(sub, [y for x,y in self.json_data[stud][sub].iteritems()]) for sub in self.json_data[stud].keys()]) for stud in self.json_data.keys()]
 
 	def __str__(self):
 		"""To string method"""
         	return self.school_name + ", year of: " + self.year + "\n"
 
-	def append_students(self, students):
-		"""Appends students list to the diary"""
-		if not isinstance(students, list):
-			print("Pass the students as a list object!!!")
-			return		
-		
-		for i in students:
-			if not isinstance(i, Student):
-				print("Incompatible types - You must provide a valid Student object")
-				return
-		self.students.extend(students)
+	def __save_and_update(self):
+		json.dump(self.json_data, open(self.filename,"w"))
+		self.students = self.__load_data(self.filename)
 
-	def get_student(self, first_name, last_name):
-		"""Returns specific student"""
-		for i in self.students:
-			if i.first_name == first_name and i.last_name == last_name:
-				print("\nSelected student:")
-				print(i)
-				return i
-		print("No matching student")
-		return
+	def add_student(self):
+		"""Adds a student to the JSON file"""
+		new_subjects = {}
+		stud_name = raw_input("Enter name: ")
+
+		print("\nAdding subjects:\n")
+		while True:
+			sub_name = raw_input("Enter subject's name: ")
+			new_grades = [float(i) for i in raw_input("Enter grades separated by space: ").split()]
+			new_attendance = [int(i) for i in raw_input("Enter attendance separated by space: ").split()]
+			new_subject = { sub_name : {"grades" : new_grades, "attendance" : new_attendance}}
+			new_subjects.update(new_subject)
+
+			if raw_input("Do You want to add another one [Y/N]? ") == "N":
+				break		
+		self.json_data.update({stud_name : new_subjects})
+		self.__save_and_update()
+
+	def delete_student(self):
+		try:		
+			del self.json_data[raw_input("Enter student's name: ")]
+			self.__save_and_update()
+		except KeyError, e:
+    			print("No matching student\n")
+
+	def open_menu(self):
+		"""Opens control menu"""
+		current_student = None
+		while True:
+			ans=raw_input(self.MENU[0])
+			print(self.MENU[2])
+			if ans=="1":
+				self.add_student()
+			elif ans=="2":
+				self.delete_student()
+			elif ans=="3":
+				self.print_students()
+			elif ans=="4":
+				current_student = self.get_student(raw_input("Enter student's name: "))
+				if current_student:
+					print(current_student)
+					while True:
+						ans=raw_input(self.MENU[1])
+						print(self.MENU[2])
+						if ans=="1":
+							print(current_student.compute_total_average())
+						elif ans=="2":
+							print(current_student.compute_subject_average(raw_input("Enter subject's name: ")))
+						elif ans=="3":
+							print(str(current_student.compute_total_attendance()) + " %") 
+						elif ans=="4":
+							print(str(current_student.compute_subject_attendance(raw_input("Enter subject's name: "))) + " %")
+						elif ans=="5":
+							break
+						else:
+							print("No such option\n")
+				else:
+					print("No matching student")
+			elif ans=="5":
+				print("Goodbye!")
+				break
+			else:
+				print("No such option\n")
+		
+	def get_student(self, student):
+		return next((s for s in self.students if s.name == student), None)
 
 	def print_students(self):
 		"""Prints students"""
@@ -55,4 +117,9 @@ class Diary(object):
 
 	@school_name.setter
 	def school_name(self, school_name):
-		self.__school_name = school_name		
+		self.__school_name = school_name
+
+
+if __name__ == "__main__":
+	diary = Diary("High school", "2016/2017", "data.json")
+	diary.open_menu()		
--- subject.py
+++ subject.py
@@ -1,13 +1,18 @@
 class Subject(object):
 	"""Class represents a single school subject"""
-	def __init__(self, name, teacher_name):
+	def __init__(self, name, data):
 		"""Default init method"""		
 		self.name = name
-		self.teacher_name = teacher_name
+		self.grades = data[0]
+		self.attendance = data[1]
+		
+	def get_average(self):
+		"""Returns average grade of a subject"""
+		return sum(self.grades)/float(len(self.grades))
 
-	def __str__(self):
-		"""To string method"""		
-        	return self.name + " (" + self.teacher_name + ")"
+	def get_attendance(self):
+		"""Returns percentage attendance of a subject"""
+		return sum(self.attendance)/float(len(self.attendance)) * 100.
 		
 	"""Getter & setter methods"""
 	@property
@@ -19,12 +24,19 @@ class Subject(object):
 		self.__name = name
 
 	@property
-	def teacher_name(self):
-		return self.__teacher_name
+	def grades(self):
+		return self.__grades
 
-	@teacher_name.setter
-	def teacher_name(self, teacher_name):
-		self.__teacher_name = teacher_name
+	@grades.setter
+	def grades(self, grades):
+		self.__grades = grades
 
+	@property
+	def attendance(self):
+		return self.__attendance
+
+	@attendance.setter
+	def attendance(self, attendance):
+		self.__attendance = attendance
 
 
